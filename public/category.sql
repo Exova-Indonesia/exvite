19696042e981b32d9360dc1c78ef9609c71ffbf6
arthaputra <manager.arthachan@gmail.com>
2021-03-28 17:05:14 +0800

    upload porto kurang redirect

---
+++ public/category.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM db_exova.category
+LIMIT 0, 1000
+
+-- Date: 2021-03-28 17:04
+*/
+INSERT INTO `` (`id`,`name`,`icon`,`created_at`,`updated_at`) VALUES (1,'Photography','',NULL,NULL);
+INSERT INTO `` (`id`,`name`,`icon`,`created_at`,`updated_at`) VALUES (2,'Videography','',NULL,NULL);
+INSERT INTO `` (`id`,`name`,`icon`,`created_at`,`updated_at`) VALUES (3,'Lainnya','',NULL,NULL);
